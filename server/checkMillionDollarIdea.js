98631098a1a46e1f06fc720ad3e768f2d0d0b57d
David Vu <davidvu7887@gmail.com>
2022-10-21 20:48:55 -0700

    Finished api routes and added middleware

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,13 @@
-const checkMillionDollarIdea = () => {};
-
+const checkMillionDollarIdea = (req, res, next) => {
+    let numWeeks = Number(req.body.numWeeks);
+    let weeklyRevenue = Number(req.body.weeklyRevenue);
+    let expectedReturn = numWeeks * weeklyRevenue;
+    
+    if (!numWeeks || !weeklyRevenue || isNaN(numWeeks) || isNaN(weeklyRevenue) || expectedReturn < 1000000) {
+        return res.status(400).send();    
+    }
+    next();
+};
+ 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
